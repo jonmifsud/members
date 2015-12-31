7b0de518c722e0c13e7076f6f6ff55763eb4ff6b
Jon Mifsud <info@jonmifsud.com>
2015-12-31 11:03:30 +0100

    Revert "role field crashes when extension not included"
    This reverts commit eabe67af3f34616ee30e1d1af52d4f630cfe830f.

--- fields/field.memberrole.php
+++ fields/field.memberrole.php
@@ -2,7 +2,6 @@
 
 	require_once TOOLKIT . '/fields/field.select.php';
 	require_once(EXTENSIONS . '/members/lib/class.role.php');
-	require_once(EXTENSIONS . '/members/extension.driver.php');
 
 	Class fieldMemberRole extends fieldSelect {
 
